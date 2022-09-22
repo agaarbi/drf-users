95071312c81502a8b9fd8beae649bed7d3cc4dda
ghani <agaarbi@gmail.com>
2022-09-22 12:39:12 +0500

    custom responses and status codes are used

--- drfusers/users/views.py
+++ drfusers/users/views.py
@@ -1,11 +1,12 @@
+from venv import create
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 from .serializer import AccountSerializer
 from .models import Accounts
 from django.contrib.auth.hashers import check_password
 
 from .jwt import get_tokens_for_user, get_user_id
+from .responses import *
 
 
 @api_view(['POST'])
@@ -14,9 +15,9 @@ def create_user(request):
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         # print(serializer.data)
-        return Response({"status": "user registered"})
+        return CustomResponse.create()
     else:
-        return Response({"invalid": "criteria not match"})
+        return CustomResponse.error(error_response="USER_DOES_NOT_CREATED", status=ERROR_STATUS_CODE_FORBIDDEN)
 
 
 @api_view(['POST'])
@@ -25,17 +26,21 @@ def login_user(request):
     login_phone = request.data.get('phone')
     login_password = request.data.get('password')
 
-    user = Accounts.objects.get(phone=login_phone)
-    if user:
-        if check_password(login_password, user.password):
-            token = get_tokens_for_user(user)
-            user_id = get_user_id(token.get('access'))
-            print(f"parsed user_id: {user_id}")
-            response_field = {"access": token.get('access'), "refresh": token.get(
-                'refresh'), "first_name": user.first_name, "last_name": user.last_name}
+    try:
+        user = Accounts.objects.get(phone=login_phone)
+        if user:
+            if check_password(login_password, user.password):
+                token = get_tokens_for_user(user)
+                # user_id = get_user_id(token.get('access'))
+                # print(f"parsed user_id: {user_id}")
+                response_field = {"access": token.get('access'), "refresh": token.get(
+                    'refresh'), "first_name": user.first_name, "last_name": user.last_name}
+            else:
+                return CustomResponse.error_login(error_response="error", wrong_attempts="", status=ERROR_STATUS_CODE)
         else:
-            print("invalid password")
-    else:
-        print("invalid phone number")
+            return CustomResponse.error(error_response="error", status=ERROR_STATUS_CODE)
 
-    return Response(response_field)
+    except Exception as err:
+        return CustomResponse.internal_server_error(str(err))
+
+    return CustomResponse.success_data(response_field)
